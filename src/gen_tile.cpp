f48e58667e4bfea2a400de5493950018a06d4fb8
Simon Annetts <simona@ateb.co.uk>
2018-11-22 11:46:03 +0000

    Fix bng 27700 projection default aspect and bounds

--- src/gen_tile.cpp
+++ src/gen_tile.cpp
@@ -120,17 +120,18 @@ struct projectionconfig * get_projection(const char * srs) {
         prj->bound_y1 =  10018754.1714;
         prj->aspect_x = 2;
         prj->aspect_y = 1;
-    } else if (strcmp(srs, "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs") == 0) {
-        syslog(LOG_DEBUG, "Using bng projection settings");
+    } else if (strcmp(srs, "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs") == 0 ||
+                strcmp(srs, "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.999601 +x_0=400000 +y_0=-100000 +ellps=airy  +units=m +towgs84=446.448,-125.157,542.060,0.1502,0.2470,0.8421,-20.4894 +units=m +nodefs") == 0) {
+        syslog(LOG_DEBUG, "Using bng EPSG:27700 projection settings");
         prj = (struct projectionconfig *)malloc(sizeof(struct projectionconfig));
-        prj->bound_x0 = 0;
-        prj->bound_y0 = 0;
-        prj->bound_x1 = 700000;
-        prj->bound_y1 = 1400000;
+        prj->bound_x0 = -350000;
+        prj->bound_y0 = -100000;
+        prj->bound_x1 = 1050000;
+        prj->bound_y1 = 1300000;
         prj->aspect_x = 1;
-        prj->aspect_y = 2;
+        prj->aspect_y = 1;
     } else {
-        syslog(LOG_WARNING, "Unknown projection string, using web mercator as never the less. %s", srs);
+        syslog(LOG_WARNING, "Unknown projection string '%s', using web mercator instead.", srs);
         prj = (struct projectionconfig *)malloc(sizeof(struct projectionconfig));
         prj->bound_x0 = -20037508.3428;
         prj->bound_x1 =  20037508.3428;
